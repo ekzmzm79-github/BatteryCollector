1d74209e0340e0d9b17154750a618ea3dc0a2229
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2022-01-31 21:18:30 +0900

    RandomSpawn Volume
    하늘에서 랜덤으로 떨어지는 배터리 구현을 위해서 SpawnVolume 클래스 구현
    When, Where, What 으로 나눠서 구현 진행

---
+++ Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Return the WhereToSpawn subobject
+	FORCEINLINE class UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; }
+
+	// Find a random point within the BoxComponent
+	UFUNCTION(BlueprintPure, Category= "Spawning")
+	FVector GetRamdomPointInVolume();
+
+protected:
+	// The pickup to spawn
+	UPROPERTY(EditAnyWhere, Category = "Spawning")
+	TSubclassOf<class APickup> WhatToSpawn;
+
+	FTimerHandle SpawnTimer;
+
+	// Minimum spawn delay
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayRangeLow;
+
+	// Maximum spawn delay
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayRangeHigh;
+
+private:
+	// Box Component to specify where pickups should be spawned
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* WhereToSpawn;
+	
+	// Handle  spawning a new pickup
+	void SpawnPickup();
+
+	// The current spawn delay
+	float SpawnDelay;
+};
